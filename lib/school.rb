c4f6b0937ae61fd2229f355eecebadf94edd937f
Justin Branch <justinms@outlook.com>
2019-12-23 02:43:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -19,10 +19,19 @@ class School
     roster[grade]  
   end   
   
-  def sort
-    roster.each {|grade, name| name.sort!}
-    sorted = roster.sort_by {|grade, name| grade}
-    sorted.to_h
-  end 
+#   def sort
+#   #   roster.each {|grade, name| name.sort!}
+#   #   sorted = roster.sort_by {|grade, name| grade}
+#   #   sorted.to_h
+#   # end 
+# end 
+
+def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    p sorted
+  end
 end 
 
