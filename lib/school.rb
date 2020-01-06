73cfb9845a34658a6343841cea3e342fcdf5e973
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-06 20:55:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(student, grade)
+  roster[grade] ||= []
+  roster[grade] << student
+end
+
+def grade(grade)
+  roster.each do |x,y|
+    if x == grade
+      then 
+      return y
+    end
+  end
+end
+  
+def sort
+  hash = {}
+  roster.each do |x, y|
+    hash[x] = y.sort
+  end
+  hash
+  
+end
+  
+  
+  
+  
+end
+
