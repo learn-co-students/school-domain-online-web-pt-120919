aad4c013b80ed3d50a9713034da4b03b7d4c5ce6
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-06 20:18:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School 
+  def initialize name 
+    @name = name 
+  end 
+  def  roster = {}
+    @roster={}
+  end 
+  def add_student = add_students
+    @add_student=add_students
+  end 
+  
+  
